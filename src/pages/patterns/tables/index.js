6b3a3cb9e53a9632733d919e491b826b8b971c5b
roge5716 <roger.avalos@rackspace.com>
2018-05-21 12:41:24 -0500

    finishing table docs, adding bootstrap some variables from zoolander

--- src/pages/patterns/tables/index.js
+++ src/pages/patterns/tables/index.js
@@ -14,7 +14,23 @@ export default Vue.component('tables', {
   },
   data() {
     return {
-      code: TableTpl,
+      code: this.clean(TableTpl),
+      jsCode: this.pluginJs(),
     };
+  },
+  methods: {
+    clean(str) {
+      // here we can strip out vue things for our code docs
+      return str.replace(/ ref="(.*)"/g, '');
+    },
+    pluginJs() {
+      // running snippets like this for highlightjs output
+      // needs to be moved to the beginning of the file or
+      // else there will be funky whitespaces
+      return `
+$.fn.flux.responsiveTable({
+  el: $('.productTable')
+});`.trim();
+    },
   },
 });
